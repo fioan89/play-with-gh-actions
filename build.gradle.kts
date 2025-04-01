d0b00e8c5b27f1b5efa57dde1740190f0856691e
Faur Ioan-Aurel <fioan89@gmail.com>
2025-04-01 21:40:05 +0300

    release workflow (1)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,8 @@
+import org.jetbrains.kotlin.com.intellij.openapi.util.SystemInfoRt
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import kotlin.io.path.createDirectories
+import kotlin.io.path.div
+import kotlin.io.path.writeText
 
 plugins {
     kotlin("jvm") version "2.1.10"
@@ -22,12 +27,114 @@ kotlin {
     jvmToolchain(21)
 }
 
-val pluginZip by tasks.creating(Zip::class) {
-    destinationDirectory.set(file("build/distributions"))
-    archiveBaseName.set("play-with-gh-actions")
-    dependsOn(tasks.jar)
-    from(tasks.jar)
-    from("src/main/resources")
-    into("com.fioan89") // folder like com.fioan89
-    // Specify the directory where the zip file will be saved
+val extension = ExtensionJson(
+    id = properties("group"),
+
+    version = properties("version"),
+    meta = ExtensionJsonMeta(
+        name = "Coder",
+        description = "Connects your JetBrains IDE to Coder workspaces",
+        vendor = "Coder Technologies, Inc.",
+        url = "https://github.com/coder/coder-jetbrains-toolbox",
+    )
+)
+
+changelog {
+    version.set(extension.version)
+    groups.set(emptyList())
+    title.set("Coder Toolbox Plugin Changelog")
 }
+
+
+tasks.compileKotlin {
+    compilerOptions.jvmTarget.set(JvmTarget.JVM_21)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+
+tasks.jar {
+    archiveBaseName.set(extension.id)
+}
+
+val copyPlugin by tasks.creating(Sync::class.java) {
+    dependsOn(tasks.jar)
+
+    fromCompileDependencies()
+}
+
+fun CopySpec.fromCompileDependencies() {
+    from(tasks.jar)
+
+    from("src/main/resources") {
+        include("dependencies.json")
+    }
+    from("src/main/resources") {
+        include("icon.svg")
+        rename("icon.svg", "pluginIcon.svg")
+    }
+
+    // Copy dependencies, excluding those provided by Toolbox.
+    from(
+        configurations.compileClasspath.map { configuration ->
+            configuration.files.filterNot { file ->
+                listOf(
+                    "kotlin",
+                    "remote-dev-api",
+                    "core-api",
+                    "ui-api",
+                    "annotations",
+                    "localization-api",
+                    "slf4j-api"
+                ).any { file.name.contains(it) }
+            }
+        },
+    )
+}
+
+/**
+ * Useful when doing manual local install.
+ */
+val pluginPrettyZip by tasks.creating(Zip::class) {
+    archiveBaseName.set(properties("name"))
+    dependsOn(tasks.jar)
+
+    fromCompileDependencies()
+    into(extension.id) // folder like com.coder.toolbox
+}
+
+val pluginZip by tasks.creating(Zip::class) {
+    dependsOn(tasks.jar)
+
+    fromCompileDependencies()
+    archiveBaseName.set(extension.id)
+}
+
+tasks.register("cleanAll", Delete::class.java) {
+    dependsOn(tasks.clean)
+    delete()
+}
+
+val publishPlugin by tasks.registering {
+    dependsOn(pluginZip)
+}
+
+fun properties(key: String) = project.findProperty(key).toString()
+
+
+// region will be moved to the gradle plugin late
+data class ExtensionJsonMeta(
+    val name: String,
+    val description: String,
+    val vendor: String,
+    val url: String?,
+)
+
+data class ExtensionJson(
+    val id: String,
+    val version: String,
+    val meta: ExtensionJsonMeta,
+)
+// endregion
